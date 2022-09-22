d5b01ffa3ef2d7a687387a65e3f10fcf5a918ca1
Vovka7Surnyk <vovka@DESKTOP-9JUS0U9.localdomain>
2022-09-22 16:47:37 +0300

    Add new log

--- bashscript.sh
+++ bashscript.sh
@@ -4,4 +4,4 @@
 
 git add *
 git commit -m "Add new log"  
-git push https://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@github.com/Vovka7Surnyk/DevOpsTask.git
+git push https://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@github.com/Vovka7Surnyk/DevOpsTask.git
