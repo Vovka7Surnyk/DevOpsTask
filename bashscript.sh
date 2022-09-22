4d6556031aa8f2ee6ebd7e50939ca5d4f13d1862
VOVKA <-MAIL>
2022-09-22 16:55:38 +0300

    Add new log

--- bashscript.sh
+++ bashscript.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 
+E-MAIL="vovka7surnyk@gmail.com"
+NAME="VOVKA"
+TOCKEN=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
 
-git config --global user.email "vovka7surnyk@gmail.com" 
-git config --global user.name "Vovka"
+git config --global user.email $E-MAIL 
+git config --global user.name $NAME
 git add *
 git commit -m "Add new log"  
-git push https://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@github.com/Vovka7Surnyk/DevOpsTask.git
+git push https://$TOCKEN@github.com/Vovka7Surnyk/DevOpsTask.git
