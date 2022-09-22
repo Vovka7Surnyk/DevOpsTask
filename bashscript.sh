85c08b3119a443665d65d3f655075ead10709123
DESKTOP-9JUS0U9 <-MAIL>
2022-09-22 17:26:59 +0300

    Add new log

--- bashscript.sh
+++ bashscript.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 
-E-MAIL="vovka7surnyk@gmail.com"
-NAME="VOVKA"
-TOCKEN=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
-REPO=github.com/Vovka7Surnyk/DevOpsTask.git
-
 git config --global user.email $E-MAIL 
 git config --global user.name $NAME
 git add *
 git commit -m "Add new log"  
-git push https://$TOCKEN@$REPO
+git push https://$TOKEN@$REPO
